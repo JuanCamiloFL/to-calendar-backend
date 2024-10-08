1535a0ff1b737080e72341f2eeba8c2f812e498d
Lau-devMl <laura.acevedog@comunidad.iush.edu.co>
2024-10-08 11:31:35 -0500

    Upgrade index.js to load data from JSON

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import sequelize from './config/db.js';
-import userRoutes from './routes/userRoutes.js';
+import apiDb from './config/apiDb.js'; // Conexión a MongoDB
+import userRoutes from './routes/userRoutes.js'; // Rutas para usuarios
+import apiRoutes from './routes/apiRoutes.js'; 
 import cors from 'cors';
+import { loadDataFromJSON } from './controllers/apiController.js'; 
 
 dotenv.config();
 
@@ -10,16 +13,22 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-
-// Middleware para parsear JSON
 app.use(express.json());
+app.use('/api', userRoutes); // Rutas de usuario
+app.use('/api', apiRoutes);
 
-// Rutas de usuario
-app.use('/api', userRoutes);
-
-// Sincronizar la base de datos y arrancar el servidor
-sequelize.sync().then(() => {
+//// Sincronizar la base de datos y arrancar el servidor
+sequelize.sync().then(async () => {
   console.log('Database synced!');
+
+  // Cargar datos desde JSON al iniciar el servidor
+  try {
+    await loadDataFromJSON(); 
+    console.log('Datos cargados correctamente.');
+  } catch (error) {
+    console.error('Error cargando datos:', error); 
+  }
+
   app.listen(port, () => {
     console.log(`Server running on port ${port}`);
   });
